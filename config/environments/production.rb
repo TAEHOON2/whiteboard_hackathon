9e5d1935b9912af57d5cf508936238a440da8231
Leopold Shim <skywoo1994@likelion.org>
2017-08-27 14:12:31 +0000

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,21 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'whiteboard-leopoldshim.c9users.io' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'whiteboardku2@gmail.com', # 화이트보드 계정
+    password:       'ghkdlxmqhem', # 화이트보드
+    domain:         'gmail.com',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
